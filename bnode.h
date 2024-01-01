d3d57ac82ac2f22dbf37a1fc801f27ef88b1e0e6
Aditya Tiwari <65213780+ENUMERA8OR@users.noreply.github.com>
2024-01-01 20:10:11 +0530

    Add files via upload

---
+++ bnode.h
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+#include<string.h>
+namespace ns3 { 
+
+class BlockNode { 
+public: 
+       int data;
+        int iotdevice;
+
+	int sensordata; 
+        
+
+	BlockNode* next; 
+}; 
+};
